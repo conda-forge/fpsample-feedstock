5109a82c2fd740394fc5f195b351387d705d7d73
Jeongseok Lee <jeongseok@meta.com>
2025-11-29 13:09:49 -0800

    Add cmake to host and force Ninja in CMAKE_ARGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,7 @@ set -ex
 # cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 
 export CMAKE_GENERATOR=Ninja
+export CMAKE_ARGS="${CMAKE_ARGS} -GNinja"
 
 # Build and install the package
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
