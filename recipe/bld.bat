564029f4b9875b7d3ea0579fd486c6895bf030d0
Jeongseok Lee <jeongseok@meta.com>
2025-11-29 22:22:32 -0800

    Fix Windows build by prepending ssize_t typedef in bld.bat

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,2 +1,9 @@
+copy src\lib.cpp src\lib.cpp.orig
+echo #if defined(_MSC_VER) > src\lib.cpp
+echo #include ^<cstddef^> >> src\lib.cpp
+echo typedef std::ptrdiff_t ssize_t; >> src\lib.cpp
+echo #endif >> src\lib.cpp
+type src\lib.cpp.orig >> src\lib.cpp
+
 set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 %PYTHON% -m pip install . --no-deps --no-build-isolation
